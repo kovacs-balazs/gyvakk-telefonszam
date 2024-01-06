1113d090876d0728ef7d4933bada8680a8a80d40
Balázs Kovács <56174644+kovacs-balazs@users.noreply.github.com>
2024-01-06 10:54:01 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from phone_number import mobile_operators_counter
 from generator import PhonenumberGenerator
 from phone_country import PhoneCountry
 import phone_country as phc
-
 import argparse
+
+def generator(function):
+    for i in range(10):
+        function()
 
 my_parser = argparse.ArgumentParser()
 my_parser.add_argument("--nogui", action="store_true", help="konzol használata")
-my_parser.add_argument("--autogenerate", action="store_true", help="Telefonszámok generálása")
-my_parser.add_argument("--file", action="store", type=argparse.FileType('r'), help="File megadása")
+my_parser.add_argument("--autogenerate", action="store_true", help="telefonszámok generálása")
+my_parser.add_argument("--file", action="store", type=argparse.FileType('r'), help="file megadása")
 
 # setup countires (exclude hungary)
 with open("phone_number_countries.json") as f:
@@ -24,10 +27,10 @@ phc.phone_countries: dict[str, PhoneCountry] = phc.get_sorted_countries()
 args = my_parser.parse_args()
 if args.autogenerate:
     phoneGen = PhonenumberGenerator()
-    phoneGen.generate_invalid_international()
-    phoneGen.generate_valid_international()
-    phoneGen.generate_invalid_hungary()
-    phoneGen.generate_valid_hungary()
+    generator(phoneGen.generate_invalid_international)
+    generator(phoneGen.generate_valid_international)
+    generator(phoneGen.generate_invalid_hungary)
+    generator(phoneGen.generate_valid_hungary)
     phoneGen.write_to_file()
 
 if not args.file is None:
